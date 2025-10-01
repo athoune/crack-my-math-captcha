85cc648775fb8b5cd8fe330fe80bdfee9793b270
Mathieu Lecarme <mathieu@garambrogne.net>
2025-10-01 17:48:43 +0200

    REST in peace.

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 import base64
 from cryptography import fernet
-from aiohttp_session import setup, get_session, session_middleware
+from aiohttp_session import setup, get_session
 from aiohttp_session.cookie_storage import EncryptedCookieStorage
 from aiohttp import web
 import aiohttp_jinja2
@@ -27,12 +27,30 @@ async def handle_submit(request: web.Request):
     return {"msg": "Yes" if str(resp) == cap else "No"}
 
 
+async def handle_api_challenge(request: web.Request):
+    c, response = some_random_operation(20)
+    return web.json_response(
+        {
+            "challenge": c,
+            "secret": request.app["fernet"].encrypt(str(response).encode()).decode(),
+        }
+    )
+
+
+async def handle_api_response(request: web.Request):
+    p = await request.json()
+    secret = int(request.app["fernet"].decrypt(p["secret"]))
+    return web.json_response(p["response"] == secret)
+
+
 app = web.Application()
 app.add_routes(
     [
         web.get("/", handle_home),
         web.post("/submit", handle_submit),
         web.static("/assets", "./assets"),
+        web.get("/api/challenge", handle_api_challenge),
+        web.post("/api/response", handle_api_response),
     ]
 )
 
@@ -40,6 +58,7 @@ aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("./templates"))
 
 fernet_key = fernet.Fernet.generate_key()
 secret_key = base64.urlsafe_b64decode(fernet_key)
+app["fernet"] = fernet.Fernet(fernet_key)
 setup(app, EncryptedCookieStorage(secret_key))
 
 if __name__ == "__main__":
